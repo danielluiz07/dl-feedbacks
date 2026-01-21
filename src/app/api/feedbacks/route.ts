0b05f3478ec66214d802a60049f1725699685473
Daniel Luiz <dnaieluiz1607@gmail.com>
2026-01-21 13:49:25 -0300

    modificacao na rota POST da API

--- src/app/api/feedbacks/route.ts
+++ src/app/api/feedbacks/route.ts
@@ -1,22 +1,38 @@
-import { NextResponse } from "next/server";
-import { prisma } from "@/lib/prisma";
+import { NextResponse } from 'next/server';
+import { prisma } from '@/lib/prisma';
+import { feedbackSchema } from '@/schemas/feedbackSchema'; 
 
 export async function GET() {
+  try {
     const feedbacks = await prisma.feedback.findMany({
-        orderBy: { createdAt: 'desc' },
-    })
-    return NextResponse.json(feedbacks)
+      orderBy: { createdAt: 'desc' },
+    });
+    return NextResponse.json(feedbacks);
+  } catch (error) {
+    return NextResponse.json({ error: "Erro ao buscar feedbacks" }, { status: 500 });
+  }
 }
 
 export async function POST(request: Request) {
-    try {
-        const {name, message} = await request.json()
-        const newFeedback = await prisma.feedback.create({
-            data: {name, message}
-        })
+  try {
+    const body = await request.json();
 
-        return NextResponse.json(newFeedback, {status: 201})
-    } catch (error) {
-        return NextResponse.json({Error: "Erro ao criar um feedback"}, {status: 500})
+    const result = feedbackSchema.safeParse(body);
+
+    if (!result.success) {
+      return NextResponse.json(
+        { errors: result.error }, 
+        { status: 400 }
+      );
     }
+    const { name, message } = result.data;
+
+    const newFeedback = await prisma.feedback.create({
+      data: { name, message },
+    });
+
+    return NextResponse.json(newFeedback, { status: 201 });
+  } catch (error) {
+    return NextResponse.json({ error: "Erro interno no servidor" }, { status: 500 });
+  }
 }
