25956fc0927b918a6b8374acca1f38a1a4e581a9
Daniel Luiz <dnaieluiz1607@gmail.com>
2026-01-21 14:28:12 -0300

    modificacao no Shemas

--- src/schemas/feedbackSchema.ts
+++ src/schemas/feedbackSchema.ts
@@ -1,11 +1,6 @@
 import z from "zod";
 
 export const feedbackSchema = z.object({
-    body: z.object({
-        name: z.string({message: "É necessário colocar um Nome"}).min(2, {message: "O nome é necessário pelo menos 2 caracteres"}),
-        message: z.string({message: "Para o envio do Forms é necessário colocar um feedback"}).min(5, "O feedback preciosa ter pelo menos 5 caracteres")
-    })
-})
-
-export type FeedbackInput = z.infer<typeof feedbackSchema>;
-
+  name: z.string().min(2, { message: "Mínimo 2 caracteres" }),
+  message: z.string().min(5, { message: "Mínimo 5 caracteres" })
+});
