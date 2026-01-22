7e824488ab1c178472d26a680b2e2422c20cda34
Daniel Luiz <dnaieluiz1607@gmail.com>
2026-01-22 09:34:02 -0300

    modificacao

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,10 @@
 import {ToastContainer} from 'react-toastify'
 import "./globals.css";
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>
+    <html lang="pt-br">
+      <body suppressHydrationWarning={true}> 
         {children}
         <ToastContainer position="top-right" />
       </body>
